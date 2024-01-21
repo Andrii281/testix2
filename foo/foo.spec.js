b4fa25589812b1248c5958d3daf67d4a1c6c78dc
Your Name <you@example.com>
2024-01-21 17:38:44 +0200

    asdas

--- foo/foo.spec.js
+++ foo/foo.spec.js
@@ -3,7 +3,7 @@ const {biggestThan} = require("./foo")
 describe('biggestThan', () => {
   describe("should return true if a greater than b", () => {
     it("should return true if a greater than b, if a = 10, b = 5", () => {
-      const a = 1
+      const a = 10
       const b = 5 
 
       const result = biggestThan(a, b)
